9f676919e499c6164ed236fe3df70d121359e387
VerneriL <44767@edu.raseko.fi>
2023-03-24 15:19:40 +0200

    Luo GUI

---
+++ main.py
@@ -0,0 +1,38 @@
+# MAIN WINDOW FOR FITNESS APPLICATION
+
+# LIBRARIES AND MODULES
+import sys
+
+from PyQt6 import QtCore # Core functionality of Qt
+from PyQt6 import QtWidgets # UI elements functionality
+from PyQt6.uic.load_ui import loadUi 
+
+
+
+# Class for the main window
+class MainWindow(QtWidgets.QMainWindow):
+    """MainWindow for the fitness app"""
+
+    # Constructor
+    def __init__(self):
+        super().__init__()
+        
+        # Load the UI file
+        loadUi('main.ui', self)
+
+        # Define UI Controls ie. buttons and input fields
+        self.calculatePB = self.calculatePushButton
+        self.calculatePB.clicked.connect(self.calculateAll)
+
+    # Define slots ie. methods.
+    def calculateAll(self):
+        self.bmiLabel.setText('100')
+
+if __name__=='__main__':
+    # Create the application
+    app = QtWidgets.QApplication(sys.argv)
+    # Create the mainwindow (and show it)
+    appWindow = MainWindow()
+    appWindow.show()
+    sys.exit(app.exec())
+
